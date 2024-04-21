12caec342df49f7ce81d2a200eed36218c0263e2
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-04-21 04:04:38 -0700

    Solve betty issues.

--- 2-simple_shell.c
+++ 2-simple_shell.c
@@ -4,39 +4,53 @@
 
 /**
  * main - Entry point to the function.
+ * @argc: Number of cmd line inputs.
+ * @argv: String list of all cmd line inputs.
  *
  * Return: 0 if program runs successfully.
  */
-int main(void)
+int main(int argc, char **argv __attribute__ ((unused)))
 {
-	char *getline_buff = NULL, buffer[BUFFER_SIZE], **env = environ, *arg[2];
+	char *getline_buff, buffer[BUFFER_SIZE], **env, *arg[2];
 	pid_t my_pid;
-	size_t getline_len = 0;
-	int count = 0, status, counter = 0;
+	size_t getline_len;
+	int count, status, counter;
 
+	env = environ;
+	getline_buff = NULL;
+	getline_len = count = counter = 0;
+	if (argc != 1)
+	{
+		perror("Usage: ./shell\n");
+		return (-1);
+	}
 	fprintf(stdout, "#cisfun$ ");
 	while (getline(&getline_buff, &getline_len, stdin) != -1)
 	{
 		count = counter = 0;
 		while (getline_buff[count] != '\n')
 		{
-			if (getline_buff[count] == ' ' && counter == 0)
+			if (getline_buff[count] == ' ')
 			{
 				count++;
 				continue;
 			}
 			buffer[counter] = getline_buff[count];
+
 			count++;
 			counter++;
 		}
 		buffer[count] = '\0';
 		arg[0] = buffer;
 		arg[1] = NULL;
+		if (count == -1)
+			continue;
 		my_pid = fork();
 		if (my_pid == -1)
 		{
 			perror("Error: Failed to fork.\n");
-			break;
+			free(getline_buff);
+			return (-1);
 		}
 		if (my_pid == 0)
 		{
@@ -44,9 +58,15 @@ int main(void)
 			fprintf(stdout, "./shell: No such file or directory\n");
 			return (1);
 		}
-		waitpid(my_pid, &status, 0);
-		fprintf(stdout, "#cisfun$ ");
+
+		if (my_pid != 0)
+		{
+			waitpid(my_pid, &status, 0);
+			getline_buff = NULL;
+			fprintf(stdout, "#cisfun$ ");
+		}
 	}
+
 	free(getline_buff);
 	return (0);
 }
